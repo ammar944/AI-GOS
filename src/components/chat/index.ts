e8450b9b84bf820b7e9911291afaf0d45cd89dc5
Ammar <ammarv67@gmail.com>
2026-01-20 17:56:20 +0500

    chore(34-02): add ChatSidebar to barrel exports
    - Export ChatSidebar from chat component index
    - Keep BlueprintChat for backwards compatibility

--- src/components/chat/index.ts
+++ src/components/chat/index.ts
@@ -4,3 +4,4 @@ export { TypingIndicator } from "./typing-indicator";
 export { QuickSuggestions } from "./quick-suggestions";
 export { ChatMessage } from "./chat-message";
 export { BlueprintChat } from "./blueprint-chat";
+export { ChatSidebar } from "./chat-sidebar";
